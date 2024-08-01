df75c66f34da30e242c3418a1108803c9178e23b
Caitlin Baldini <cat@listoglobal.com>
2024-08-01 11:03:40 -0600

    working through problem

--- WhereAreMyGlasses.js
+++ WhereAreMyGlasses.js
@@ -9,7 +9,18 @@
 // All elements in the list are strings.
 
 function findGlasses(arr) {
-  //look for the index where the first zero has a dash on the right and second has dash on the left
-  for (let i = 0; i < arr.length; i++) {}
+  //look for the index where the first zero has a dash on the right and second has dash on the left.
+  for (let i = 0; i < arr.length; i++) {
+    // if index is 0 loop through until you hit another 0.
+    if (arr[i] === "0") {
+      let j = i + 1;
+      while (j < arr.length && arr[j] === "-") {
+        j++; // it is now incrementing through the new inner loop
+      }
+      if (j < arr.length && arr[j] === "0") {
+        return i;
+      }
+    }
+  }
 }
 //return the index
